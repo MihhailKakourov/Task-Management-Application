fad0af91a4176133ac615a01e7903828fff8a04a
Eduard <your@example.com>
2024-02-09 10:20:39 +0200

    HTML Templates and MenuController added

--- src/main/java/com/example/todolist/Controllers/MenuController.java
+++ src/main/java/com/example/todolist/Controllers/MenuController.java
@@ -11,4 +11,21 @@ public class MenuController {
         model.addAttribute("header", "ToDoList");
         return "index";
     }
+    @GetMapping("/contacts")
+    public String contactPage(Model model) {
+        model.addAttribute("header", "ToDoList");
+        return "contacts";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(Model model) {
+        model.addAttribute("header", "ToDoList");
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signUpPage(Model model) {
+        model.addAttribute("header", "ToDoList");
+        return "signup";
+    }
 }
